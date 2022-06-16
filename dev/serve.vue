86d36bb9ca51335603b4ac82f8f3cd2929308f85
Скатов Дмитрий Андреевич <SkatovDA@dpractices.local>
2022-06-16 18:04:45 +0300

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import PlanRIco from '@/plan-r-ico.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    PlanRIco
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <plan-r-ico class-icon="Up"/>
+  </div>
+</template>
